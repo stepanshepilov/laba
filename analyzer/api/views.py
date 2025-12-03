82f033b0af5a2afd243776d831f5977c0b577713
Stephan Shepilov <stepanshepilovwork@gmail.com>
2025-12-03 12:24:11 +0700

    Change: 4 lab

--- analyzer/api/views.py
+++ analyzer/api/views.py
@@ -1,22 +1,31 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 from aiokafka import AIOKafkaProducer
+from pydantic import BaseModel
+from loguru import logger
+from uuid import uuid4
+import asyncio
+import redis
 import json
 import os
-import asyncio
-from loguru import logger
 
 app = FastAPI()
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9094')
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 TOPIC_NAME = 'sentiment_tasks'
+
+redis_client = redis.Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
+num_partitions = 10 # так же, как и в compose файле
+producer = None
 
 class PredictRequest(BaseModel):
     text: str
 
 class PredictResponse(BaseModel):
+    task_id: str
     status: str
-    message: str
+
+task_counter = 0
 
 producer = None
 @app.on_event("startup")
@@ -47,7 +56,24 @@ async def shutdown_event():
 
 @app.post("/predict", response_model=PredictResponse)
 async def predict(request: PredictRequest):
-    message = json.dumps({"text": request.text}).encode("utf-8")
+    global task_counter
+    task_id = str(uuid4())
+    redis_client.set(task_id, 'processing')
 
-    await producer.send_and_wait(TOPIC_NAME, message)
-    return {"status": "queued", "message": "Task sent to workers"}
+    message = json.dumps({"task_id": task_id, "text": request.text}).encode("utf-8")
+    partition_id = task_counter % num_partitions
+    task_counter += 1
+
+    await producer.send_and_wait(TOPIC_NAME, message, partition=partition_id)
+    logger.info(f'Send task {task_counter - 1} to partition {partition_id}')
+
+    return PredictResponse(task_id=task_id, status='queued')
+
+@app.get("task/{task_id}", response_model=PredictResponse)
+async def get_result(task_id: str):
+    status = redis_client.get(task_id)
+
+    if not status:
+        raise HTTPException(status_code=404, detail='Task is not found')
+    
+    return PredictResponse(task_id=task_id, status=status)
--- analyzer/worker/model.py
+++ analyzer/worker/model.py
@@ -1,19 +1,21 @@
-import os
-import json
-import asyncio
-import socket
 from aiokafka import AIOKafkaConsumer
 from textblob import TextBlob
 from loguru import logger
+import asyncio
+import socket
+import redis
+import json
+import os
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9094')
 TOPIC_NAME = 'sentiment_tasks'
 GROUP_ID = os.getenv('GROUP_ID', 'my_group')
-
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 WORKER_ID = socket.gethostname()
+redis_client = redis.Redis(REDIS_HOST, port=6379, db=0, decode_responses=True)
 
 async def consume():
-    print(f"Worker {WORKER_ID} starting...")
+    logger.info(f"Worker {WORKER_ID} starting...")
     
     consumer = None
     retry_count = 0
@@ -22,12 +24,16 @@ async def consume():
     while retry_count < max_retries:
         try:
             logger.info(f"Worker {WORKER_ID} connecting to Kafka ({retry_count + 1}/{max_retries})...")
+            
             consumer = AIOKafkaConsumer(
                 TOPIC_NAME,
                 bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
                 group_id=GROUP_ID,
-                auto_offset_reset='latest'
+                auto_offset_reset='earliest',
+                enable_auto_commit=False,
+                max_poll_interval_ms=300000
             )
+
             await consumer.start()
             logger.info(f"Worker {WORKER_ID} connected!")
             break
@@ -47,15 +53,24 @@ async def consume():
         async for message in consumer:
             data = json.loads(message.value.decode('utf-8'))
             text = data.get('text', '')
+            task_id = data.get('task_id')
+
+            # мы имитируем долгую работу сервиса
+            await asyncio.sleep(10)
 
             blob = TextBlob(text)
             sentiment = blob.sentiment.polarity
             result = "Happy happy happy" if sentiment > 0 else "Dead inside" if sentiment < 0 else "Niche tak"
 
+            if task_id:
+                redis_client.set(task_id, result)
+            
+            await consumer.commit()
             logger.info(f"SERVICE_TAG:WORKER_DONE:{WORKER_ID}")
         
     except Exception as e:
-        logger.exception("Беда приключилась")
+        logger.exception(f"Беда приключилась: {e}")
+        asyncio.sleep(5)
     
     finally:
         await consumer.stop()
